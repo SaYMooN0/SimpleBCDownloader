1b4fff4999ebb4d2947fb8d58bd931b553e8cb5f
SaYMooN <shokoladnysaymoon@mail.ru>
2025-10-14 23:04:25 +0300

    Project init. Added basic app logger

---
+++ AppLogger.cs
@@ -0,0 +1,86 @@
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace SimpleBCDownloader;
+
+public static class AppLogger
+{
+    private enum Level
+    {
+        IntermediateSuccess,
+        Success,
+        Warning,
+        Error
+    }
+
+
+    // tried to repoduce ILogger format 
+
+    public static void IntermediateSuccess(string messageTemplate, params object[] args)
+        => Log(Level.IntermediateSuccess, messageTemplate, args);
+
+
+    public static void Success(string messageTemplate, params object[] args)
+        => Log(Level.Success, messageTemplate, args);
+
+
+    public static void Warning(string messageTemplate, params object[] args)
+        => Log(Level.Warning, messageTemplate, args);
+
+    public static void Error(string messageTemplate, params object[] args)
+        => Log(Level.Error, messageTemplate, args);
+
+
+    private static void Log(Level level, string messageTemplate, params object[] args) {
+        string message = RenderMessage(messageTemplate, args);
+        ConsoleColor oldConsoleColor = Console.ForegroundColor;
+        Console.ForegroundColor = GetColor(level);
+        Console.Write($"[{level.ToString()}] ");
+        Console.WriteLine(message);
+        Console.ForegroundColor = oldConsoleColor;
+    }
+
+    private static ConsoleColor GetColor(Level level) => level switch {
+        Level.IntermediateSuccess => ConsoleColor.Cyan,
+        Level.Success => ConsoleColor.Green,
+        Level.Warning => ConsoleColor.Yellow,
+        Level.Error => ConsoleColor.Red,
+        _ => throw new ArgumentOutOfRangeException(nameof(level), level, null)
+    };
+
+    private static readonly Regex HoleRegex = new(@"\{[^{}]+\}", RegexOptions.Compiled);
+    const string OpenBraceTempPlug = "\uFFF0";
+    const string CloseBraceTempPlug = "\uFFF1";
+
+    private static string UnescapeDoubleBraces(string s)
+        => s.Replace("{{", "{").Replace("}}", "}");
+
+    private static string RenderMessage(string template, object[] args) {
+        if (args is null || args.Length == 0) {
+            return UnescapeDoubleBraces(template);
+        }
+
+
+        string str = template
+            .Replace("{{", OpenBraceTempPlug)
+            .Replace("}}", CloseBraceTempPlug);
+
+        int index = 0;
+        str = HoleRegex.Replace(str, _ => {
+            var i = index++;
+            return "{" + i.ToString(CultureInfo.InvariantCulture) + "}";
+        });
+
+        str = str
+            .Replace(OpenBraceTempPlug, "{")
+            .Replace(CloseBraceTempPlug, "}");
+
+        try {
+            return string.Format(CultureInfo.InvariantCulture, str, args);
+        }
+        catch {
+            //if there are more placeholders than args
+            return UnescapeDoubleBraces(template);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace SimpleBCDownloader;
+
+
+class Program
+{
+    static async Task Main(string[] args) {
+        // "https://vbeo.bandcamp.com/album/--17"
+
+        CancellationToken ct = new CancellationToken();
+        if (args.Length == 0) {
+            AppLogger.Error("No album link provided");
+            return;
+        }
+
+        string albumLink = args[0]?.Trim() ?? string.Empty;
+        if (
+            string.IsNullOrWhiteSpace(albumLink)
+            || !albumLink.StartsWith("http")
+            || !albumLink.Contains(".bandcamp.com/album")
+        ) {
+            AppLogger.Error(
+                "Invalid album link provided. Album link must be first argument and lead to an album. For example \"https://vbeo.bandcamp.com/album/--17\""
+            );
+            return;
+        }
+
+        AppLogger.IntermediateSuccess("Download started. Album link: {1}", albumLink);
+
+
+        await DownloadAlbumAsync(albumLink, ct);
+        AppLogger.Success("Download finished");
+    }
+
+    private static async Task DownloadAlbumAsync(string url, CancellationToken ct) {
+        AppLogger.Error("Not implemented yet");
+    }
+}
